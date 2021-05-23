e83f5ae90c5d7d7e37f68d231741ddd60cc1044f
Arthur <arthur@arthurvardevanyan.com>
2021-05-22 21:06:39 -0400

    recaptcha
    Former-commit-id: c0db7b3c9c6b3aa5e946b99b58d54a7fc38dc0c5

--- email.php
+++ email.php
@@ -1,57 +1,75 @@
 <?php
-if (isset($_POST['email'])) {
-  $errors = '';
-  $myemail = 'arthurva@arthurvardevanyan.com'; //<-----Put Your email address here.
-  if (
-    empty($_POST['name'])  ||
-    empty($_POST['email']) ||
-    empty($_POST['message'])
-  ) {
-    $errors .= "\n Error: all fields are required";
-  }
+if (isset($_POST['email']) && isset($_POST['recaptcha_response'])) {
 
-  $name = $_POST['name'];
-  $email_address = $_POST['email'];
-  $message = $_POST['message'];
+  // Build POST request:
+  $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+  $recaptcha_secret = '***REMOVED***';
+  $recaptcha_response = $_POST['recaptcha_response'];
 
-  if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-    $email_address
-  )) {
-    $errors .= "\n Error: Invalid email address";
-  }
+  // Make and decode POST request:
+  $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+  $recaptcha = json_decode($recaptcha);
 
-  if (empty($errors)) {
+  // Take action based on the score returned:
+  if ($recaptcha->score >= 0.5) {
+    $errors = '';
+    $myEmail = 'arthurva@arthurvardevanyan.com'; //<-----Put Your email address here.
+    if (
+      empty($_POST['name'])  ||
+      empty($_POST['email']) ||
+      empty($_POST['message'])
+    ) {
+      $errors .= "\n Error: all fields are required";
+    }
 
-    $to = $myemail;
+    $name = $_POST['name'];
+    $email_address = $_POST['email'];
+    $message = $_POST['message'];
 
-    $email_subject = "Contact form submission: $name";
+    if (!preg_match(
+      "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+      $email_address
+    )) {
+      $errors .= "\n Error: Invalid email address";
+    }
 
-    $email_body = "You have received a new message. " .
+    if (empty($errors)) {
 
-      " Here are the details:\n Name: $name \n " .
+      $to = $myEmail;
 
-      "Email: $email_address\n Message \n $message";
+      $email_subject = "Contact form submission: $name";
 
-    $headers = "From: $myemail\n";
+      $email_body = "You have received a new message. " .
 
-    $headers .= "Reply-To: $email_address";
+        " Here are the details:\n Name: $name \n " .
 
-    mail($to, $email_subject, $email_body, $headers);
+        "Email: $email_address\n Message \n $message";
+
+      $headers = "From: $myEmail\n";
+
+      $headers .= "Reply-To: $email_address";
+
+      mail($to, $email_subject, $email_body, $headers);
 
 
-    //Email Copy To Submitter
-    //$headers .= "Reply-To: $myemail";
-    //$to = $email_address;
-    //mail($to, $email_subject, $email_body, $headers);
+      //Email Copy To Submitter
+      //$headers .= "Reply-To: $myEmail";
+      //$to = $email_address;
+      //mail($to, $email_subject, $email_body, $headers);
 
 
-    //redirect to the 'thank you' page
+      //redirect to the 'thank you' page
 
-    //header('Location: index.html');
+      //header('Location: index.html');
 
-    echo '<script type="text/javascript">',
-      'window.location="index.html#emailsent";',
+      echo '<script type="text/javascript">',
+      'window.location="index.html#emailSent";',
       '</script>';
+    }
+  } else {
+    // Not verified - show form error
+    echo '<script type="text/javascript">',
+    'window.location="index.html#emailFailed";',
+    '</script>';
   }
 }
--- js/main.js
+++ js/main.js
@@ -16,21 +16,26 @@
 }
 */
 $(function () {
-  if (window.location.hash === "#emailsent") {
+  if (window.location.hash === "#emailSent") {
     alert("Email Sent!");
   }
 });
+$(function () {
+  if (window.location.hash === "#emailFailed") {
+    alert("Captcha Failed!");
+  }
+});
 
-$(document).ready(function(){ 
-  $(window).scroll(function(){ 
-      if ($(this).scrollTop() > 100) { 
-          $('#scroll').fadeIn(); 
-      } else { 
-          $('#scroll').fadeOut(); 
-      } 
-  }); 
-  $('#scroll').click(function(){ 
-      $("html, body").animate({ scrollTop: 0 }, 600); 
-      return false; 
-  }); 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn();
+    } else {
+      $('#scroll').fadeOut();
+    }
+  });
+  $('#scroll').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
