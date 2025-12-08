c202c66628b52d5986876fb0d2abdfc17a74a79a
Arthur <arthur@arthurvardevanyan.com>
2025-12-08 00:05:54 +0000

    chore(work experience): update && linting

--- kodata/js/print_img.js
+++ kodata/js/print_img.js
@@ -1,5 +1,4 @@
-
-(jQuery);
+jQuery;
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal1");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -7,18 +6,18 @@ var img = document.getElementById("img1");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption1");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal2");
@@ -27,19 +26,18 @@ var img = document.getElementById("img2");
 var modalImg = document.getElementById("img02");
 var captionText = document.getElementById("caption2");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal3");
@@ -48,19 +46,18 @@ var img = document.getElementById("img3");
 var modalImg = document.getElementById("img03");
 var captionText = document.getElementById("caption3");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal4");
@@ -69,19 +66,18 @@ var img = document.getElementById("img4");
 var modalImg = document.getElementById("img04");
 var captionText = document.getElementById("caption4");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal5");
@@ -90,19 +86,18 @@ var img = document.getElementById("img5");
 var modalImg = document.getElementById("img05");
 var captionText = document.getElementById("caption5");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal6");
@@ -111,19 +106,18 @@ var img = document.getElementById("img6");
 var modalImg = document.getElementById("img06");
 var captionText = document.getElementById("caption6");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal7");
@@ -132,19 +126,18 @@ var img = document.getElementById("img7");
 var modalImg = document.getElementById("img07");
 var captionText = document.getElementById("caption7");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal8");
@@ -153,18 +146,18 @@ var img = document.getElementById("img8");
 var modalImg = document.getElementById("img08");
 var captionText = document.getElementById("caption8");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal9");
@@ -173,18 +166,18 @@ var img = document.getElementById("img9");
 var modalImg = document.getElementById("img09");
 var captionText = document.getElementById("caption9");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal10");
@@ -193,19 +186,18 @@ var img = document.getElementById("image10");
 var modalImg = document.getElementById("img10");
 var captionText = document.getElementById("caption10");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal11");
@@ -214,19 +206,18 @@ var img = document.getElementById("image11");
 var modalImg = document.getElementById("img11");
 var captionText = document.getElementById("caption11");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
-
+  modal.style.display = "none";
+};
 
 // Get the modal https://www.w3schools.com/howto/howto_css_modal_images.asp
 var modal = document.getElementById("modal12");
@@ -235,48 +226,48 @@ var img = document.getElementById("image12");
 var modalImg = document.getElementById("img12");
 var captionText = document.getElementById("caption12");
 img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
 
 // Get the <span> element that closes the modal
-var span = modalImg
+var span = modalImg;
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = window.innerHeight - 150 + "px";
-            content.style.overflow = "auto";
-        }
-    });
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = window.innerHeight - 150 + "px";
+      content.style.overflow = "auto";
+    }
+  });
 }
 (() => {
-    'use strict';
-    // Page is loaded
-    const objects = document.getElementsByClassName('asyncImage');
-    Array.from(objects).map((item) => {
-        // Start loading image
-        const img = new Image();
-        img.src = item.dataset.src;
-        // Once image is loaded replace the src of the HTML element
-        img.onload = () => {
-            item.classList.remove('asyncImage');
-            return item.nodeName === 'IMG' ?
-                item.src = item.dataset.src :
-                item.style.backgroundImage = `url(${item.dataset.src})`;
-        };
-    });
+  "use strict";
+  // Page is loaded
+  const objects = document.getElementsByClassName("asyncImage");
+  Array.from(objects).map((item) => {
+    // Start loading image
+    const img = new Image();
+    img.src = item.dataset.src;
+    // Once image is loaded replace the src of the HTML element
+    img.onload = () => {
+      item.classList.remove("asyncImage");
+      return item.nodeName === "IMG"
+        ? (item.src = item.dataset.src)
+        : (item.style.backgroundImage = `url(${item.dataset.src})`);
+    };
+  });
 })();
